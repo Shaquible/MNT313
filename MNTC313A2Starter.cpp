8b75d873725a4ec2cf9c043f6f4cbc6010f88d59
Keegan Kelly <keegkelly2013@gmail.com>
2021-05-17 14:42:41 -0400

    week 1

---
+++ MNTC313A2Starter.cpp
@@ -0,0 +1,16 @@
+//MNTC313 Assignment 2 Starter Code
+//You will need to add headers, variables, calculations and print statements to complete the code
+//Variables for user input have been provided already
+//Do not alter the user input statement unless you are comfortable with user input already!
+
+
+
+int main()
+{
+	
+	float pLength, pWidth, pDepth, pRadius; 
+	
+	scanf("%f, %f, %f, %f", &pLength, &pWidth, &pDepth, &pRadius);
+	//In the console, enter length, width, depth and radius on a single line, each separated by a comma and space
+	
+}
