b3771ddf2dc4f8ecfdfcfb639317155b2c006180
Shaquible <67400233+Shaquible@users.noreply.github.com>
2021-05-24 14:31:50 -0400

    Add files via upload

--- projectile.cpp
+++ projectile.cpp
@@ -1,42 +1,33 @@
-//MNTC313 Assignment 3 Starter Code
-//You will need to add headers, variables, calculations and print statements to complete the code
-//Variables for user input have been provided already
-//Do not alter the user input statement unless you are comfortable with user input already!
-
 #include<stdio.h>
 #include<math.h>
+
 int main()
 {
 	float time, initVel,initAng,initH;
 	float pi = 3.14;
 	scanf("%f, %f, %f, %f", &time, &initVel, &initAng, &initH);
-	if (time < 0)
-	{
+//checks if time is positive and prints a statement and ends the program if its negative
+	if (time < 0){
 		printf("Projectile not released\n");
 		return 0;
 	}
-	printf("Projectile released\n");
-	initAng = initAng * pi/180;
-	float vy = initVel * sin(initAng);
-	vy = vy - (9.81 * time);
-	float y = initVel * sin(initAng)* time - (4.905 * pow(time,2)) + initH;
-	printf("Position: %.2f meters\nVelocity value: %.2f meters/second\n", y, vy);
-	if (vy >= 0)
-	{
+	printf("Projectile released\n"); //prints statement if time is positive
+	initAng = initAng * pi/180; //converts from deg to rad
+	float vy = initVel * sin(initAng); //converts scalar velocity to vertical component
+	vy = vy - (9.81 * time); //updates y velocity to the value at the given time
+	float y = initVel * sin(initAng)* time - (4.905 * pow(time,2)) + initH; //computes position from velocity time and initial position
+	printf("Position: %.2f meters\nVelocity value: %.2f meters/second\n", y, vy); //prints velocity and position
+//prints acending or decending and above or below initial position based on the vertical velocity and position
+	if (vy >= 0){
 		printf("Projectile ascending\n");
 	}
-	else
-	{
-     printf("Projectile descending\n");
-		}
-	if (y >= initH)
-	{
+	else{
+     	printf("Projectile descending\n");
+	}
+	if (y >= initH){
 		printf("Projectile above initial position\n");
 	}
-	else
-	{
+	else{
 		printf("Projectile below initial position\n");
 	}
-	//In the console, enter time, initial velocity, initial angle and initial heigth values on a single line separated by a comma and space. 
-	
 }
