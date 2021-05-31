e2aadf245836d424928d4a5f1cb0924e3721854b
Shaquible <67400233+Shaquible@users.noreply.github.com>
2021-05-31 00:27:49 -0400

    Add files via upload

---
+++ waterTower.cpp
@@ -0,0 +1,14 @@
+//MNTC313 Assignment 4 Starter Code
+//You will need to add headers, variables, calculations and print statements to complete the code
+//Variables for user input have been provided already
+//Do not alter the user input statement unless you are comfortable with user input already!
+
+int main()
+{
+	float initialHeight, tankRad, pipeRad;
+	int finalT;
+	
+	scanf("%f, %f, %f, %d", &initialHeight, &tankRad, &pipeRad, &finalT);
+	//In the console, enter initial height, tank radius, pipe radius and final time separated by a comma and a space
+	
+}
